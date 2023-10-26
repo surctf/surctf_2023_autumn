fb1606bd4fed2d8ace8b8a6f6278ef79bce581c5
pigadoor <30825714+pigadoor@users.noreply.github.com>
2023-10-27 01:01:47 +0300

    Add files via upload

---
+++ tasks/crypto/whynot/dec.py
@@ -0,0 +1,45 @@
+from Crypto.Util.strxor import strxor
+import string
+
+def is_valid(s):
+    valid_chars = set(string.ascii_letters + string.digits + string.punctuation)
+    return all(char in valid_chars or char.isspace() for char in s)
+
+
+with open("output.txt", "r") as f:
+    text = f.readline()
+
+unhex_text = b''.fromhex(text)
+
+len_unhex_text = len(unhex_text)
+
+koala =[]
+
+for i in range(13, 38):
+    if len_unhex_text % i == 0:
+        a={}
+
+        len_flag = len_unhex_text // i
+        for j in range(len_unhex_text):
+            if j % i == 0:
+                a[j % len_flag] = unhex_text[j]
+
+
+        if len_flag == len(a):
+            items = (dict(sorted(a.items()))).values()
+            output=b''
+            for m in items:
+                output += bytes([m])
+
+
+            for jopa in range(0,256):
+                flag = strxor(output, bytes([jopa])*len_flag)
+
+                try:
+                    string_flag = flag.decode('utf-8')
+                except UnicodeDecodeError:
+                    continue
+
+                for word in string_flag.split():
+                    if is_valid(word):
+                        print(word)
