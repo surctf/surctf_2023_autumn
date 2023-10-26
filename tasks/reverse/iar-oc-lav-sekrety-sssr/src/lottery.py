8d92102bc1c544a209b0326ff0536cde13028661
Igor Kiziridi <45760557+CornaElicottero@users.noreply.github.com>
2023-10-26 14:39:22 +0300

    Update lottery.py

--- tasks/reverse/iar-oc-lav-sekrety-sssr/src/lottery.py
+++ tasks/reverse/iar-oc-lav-sekrety-sssr/src/lottery.py
@@ -12,20 +12,3 @@ if num > 0:
                             if num % 89 == 0:
                                 if num % 8 != 0:
                                     print("surctf_a_k4k_3to_sk0mpil1t")
-                                else:
-                                    print("num % 8 != 0")
-
-                            else:
-                                print("num % 89 == 0")
-                        else:
-                            print("num % 17 == 0 and i % 19 == 0) or (num % 23 == 0 or num % 29 == 0")
-                    else:
-                        print("num % 5 == 0 or num % 11 == 0")
-                else:
-                    print("num % 3 != 0")
-            else:
-                print("num % 7 == 0")
-        else:
-            print("num % sum_of_digits == 0")
-    else:
-        print("len(num_str) >= 6 and len(num_str) <= 9")
